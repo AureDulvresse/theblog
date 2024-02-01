71b6e6615cd37d02854372a72c64eb9fe46d61fb
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-02-01 12:02:05 +0100

    Commit à revoir

--- frontend/src/components/blog/PostList.jsx
+++ frontend/src/components/blog/PostList.jsx
@@ -1,17 +1,32 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState, useEffect } from "react";
-import Post from "./Post";
+import React from "react";
+
+import { useState, useEffect } from "react";
 import axios from "axios";
+import { ReactPaginate } from "react-paginate";
+
+import Post from "./Post";
 
 // eslint-disable-next-line react/prop-types
 const PostList = ({ filter }) => {
   const [posts, setposts] = useState([]);
+  const [currentPosts, setCurrentPosts] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOfset] = useState(0);
 
   let url;
 
+  const postPerPage = 8;
+
   useEffect(() => {
     getPosts();
-  }, []);
+  });
+
+  useEffect(() => {
+    const endOffSet = itemOffset + postPerPage;
+    setCurrentPosts(posts.slice(itemOffset, endOffSet));
+    setPageCount(Math.ceil(posts.length / postPerPage));
+  }, [itemOffset, postPerPage, posts]);
 
   if (filter == 0) {
     url = "http://localhost:8000/blog/";
@@ -25,13 +40,28 @@ const PostList = ({ filter }) => {
     setposts(data);
   };
 
+  const handlePageClick = (event) => {
+    const newOffset = event.selected % posts.length;
+    setItemOfset(newOffset);
+  };
+
   return (
     <>
       <div className="grid grid-cols-3 gap-8">
-        {posts.map((post, index) => (
+        {currentPosts.map((post, index) => (
           <Post key={index} post={post} />
         ))}
       </div>
+
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+      />
     </>
   );
 };
